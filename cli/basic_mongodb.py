5d2ff499851015219ff6f65d6db983a0575c0cab
chenjj <whucjj@gmail.com>
2012-11-04 22:40:17 -0800

    add config modules to basic_mongodb.py

--- cli/basic_mongodb.py
+++ cli/basic_mongodb.py
@@ -3,15 +3,18 @@
 import sys
 import logging
 logging.basicConfig(level=logging.WARNING)
+
 from  gridfs  import GridFS
 import hpfeeds
 import pymongo
 import ast
 import datetime
 import md5
+from ConfigParser import ConfigParser
 
 HOST = '127.0.0.1'
 PORT = 10000
+CONFIGFILE = "config.cfg"
 CHANNELS = ['dionaea.connections', 'geoloc.events','dionaea.dcerpcrequests','dionaea.shellcodeprofiles','mwbinary.dionaea.sensorunique','dionaea.capture']
 IDENT = 'ww3ee@hp1'
 SECRET = '7w35rippuhx7704h'
@@ -19,11 +22,24 @@ SECRET = '7w35rippuhx7704h'
 # Required
 MONGOHOST = '127.0.0.1'
 MONGOPORT = 27017
-MONGODBNAME = 'dionaea'
+MONGODBNAME = 'hpfeeds'
 # Optional
 MONGOUSER = ''
 MONGOPWD = ''
 
+def get_config():
+	config=ConfigParser()
+	config.read(CONFIGFILE)
+	HOST=config.get("hpfeeds","HOST")
+	PORT=config.get("hpfeeds","PORT")
+	CHANNELS=config.get("hpfeeds","CHANNELS")
+	IDENT=config.get("hpfeeds","IDENT")			
+	SECRET=config.get("hpfeeds","SECRET")	
+	MONGOHOST=config.get("database","MONGOHOST")	
+	MONGOPORT=config.get("database","MONGOPORT")	
+	MONGOUSER=config.get("database","MONGOUSER")	
+	MONGOPWD=config.get("database","MONGOPWD")
+	
 def get_db(host, port, name, user = '', passwd = ''):
         dbconn = pymongo.Connection(host, port)
         db = pymongo.database.Database(dbconn, name)
@@ -33,11 +49,11 @@ def get_db(host, port, name, user = '', passwd = ''):
 
 
 def main():
+	get_config()
 	hpc = hpfeeds.new(HOST, PORT, IDENT, SECRET)
 	print >>sys.stderr, 'connected to', hpc.brokername
 
-	insertCon = pymongo.Connection(host="localhost",port=27017)
-	db = None
+	db = get_db(MONGOHOST, MONGOPORT, MONGODBNAME, MONGOUSER, MONGOPWD)
 	collection = None
 	
 	def on_message(identifier, channel, payload):
@@ -51,8 +67,7 @@ def main():
 				msg['rport'] = int(msg['rport'])
 				msg['lport'] = int(msg['lport'])
 				print 'inserting...', msg
-				db = insertCon['dionaea']
-				collection = db['connection']
+				collection = db['dionaea.connections']
 				collection.insert(msg)
 		elif channel == 'geoloc.events':
 			try:
@@ -63,8 +78,7 @@ def main():
 			else:
 				msg['time'] = datetime.datetime.strptime(msg['time'], "%Y-%m-%d %H:%M:%S")
 				print 'inserting...', msg
-				db = insertCon['geoloc']
-				collection =  db['events']
+				collection =  db['geoloc.events']
 				collection.insert(msg)
 		elif channel == 'dionaea.dcerpcrequests':
 			try:
@@ -76,8 +90,7 @@ def main():
 				dt = datetime.datetime.now()
 				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
 				print 'inserting...', msg
-				db = insertCon['dionaea']
-				collection = db['dcerpcrequests']
+				collection = db['dionaea.dcerpcrequests']
 				collection.insert(msg)
 		elif channel == 'dionaea.shellcodeprofiles':
 			try:
@@ -89,8 +102,7 @@ def main():
 				dt = datetime.datetime.now()
 				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
 				print 'inserting...', msg
-				db = insertCon['dionaea']
-				collection = db['shellcodeprofiles']
+				collection = db['dionaea.shellcodeprofiles']
 				collection.insert(msg)
 		elif channel == 'mwbinary.dionaea.sensorunique' :
 			try:
@@ -102,9 +114,7 @@ def main():
 				hash.update(payload_python)
 				msg = hash.hexdigest()
 				print 'inserting mwbinary...', msg
-				
-				db = insertCon['dionaea']
-				gfsDate=GridFS(db)
+				gfsDate=GridFS(db,'dionaea.sensorunique')
 				gfsDate.put(payload_python,filename=msg)
 		elif channel == 'dionaea.capture':
 			try:
@@ -116,9 +126,8 @@ def main():
 				dt = datetime.datetime.now()
 				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
 				print 'inserting...', msg
-				db = insertCon['dionaea']
-				collection = db['capture']
-				collection.insert(msg)		
+				collection = db['dionaea.capture']
+				collection.insert(msg)
 	def on_error(payload):
 		print >>sys.stderr, ' -> errormessage from server: {0}'.format(payload)
 		hpc.stop()
